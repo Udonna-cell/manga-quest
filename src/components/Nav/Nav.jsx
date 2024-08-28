aa8b813d17f9523a46c967a6c585655291b11fae
udonna-cell <orabuezestanley@gmail.com>
2024-08-28 08:52:28 +0100

    done on nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import notification from "../../assets/icons/clarity--notification-line.svg";
+import profile from "../../assets/icons/simple-line-icons--user.svg";
+import styles from "./Nav.module.css"
+
+function Nav({section}) {
+    return (<nav className={styles.nav}>
+        <h1>It&rsquo;s fun time!</h1>
+        <section>
+            <div className="notification">
+                <img src={notification} alt="notify-icon" />
+            </div>
+            <div className="profile">
+                <img src={profile} alt="profile-icon" />
+            </div>
+        </section>
+    </nav>)
+}
+
+export default Nav
