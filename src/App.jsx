fd9c84762adb34a96674cef11bbbd26cbd503592
udonna-cell <orabuezestanley@gmail.com>
2024-08-29 09:31:53 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import Nav from "./components/Nav/Nav"
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.scss";
+import Nav from "./components/Nav/Nav";
+import Reading from "./components/Reading/Reading";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (<Nav />)
+  // const [count, setCount] = useState(0)
+  let notice = {
+    len: 5,
+  };
+  return (
+    <>
+      <Nav notice={notice} />
+      <Reading />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,11 +2,14 @@ import notification from "../../assets/icons/clarity--notification-line.svg";
 import profile from "../../assets/icons/simple-line-icons--user.svg";
 import styles from "./Nav.module.css"
 
-function Nav({section}) {
+function Nav({notice}) {
     return (<nav className={styles.nav}>
         <h1>It&rsquo;s fun time!</h1>
         <section>
             <div className="notification">
+                <div className={styles.alert}>
+                    {notice.len}
+                </div>
                 <img src={notification} alt="notify-icon" />
             </div>
             <div className="profile">
