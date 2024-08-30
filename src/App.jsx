2bb05b67ed6bc03269f61660080ea74ad4d54685
Stanley-Orabueze <67174967+Udonna-cell@users.noreply.github.com>
2024-08-30 01:46:34 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
 import Nav from "./components/Nav/Nav"
+import Reading from "./components/Nav/Reading"
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (<Nav />)
+  return (<>
+    <Nav />
+    <Reading />
+  </>)
 }
 
 export default App
